b1c8a05be26fb65f64bfb21f533c0c33e87a096f
mnorris1215 <mnorris1215@yahoo.com>
2022-05-05 15:55:00 -0500

    add smooth scroll

--- content/_index.md
+++ content/_index.md
@@ -3,17 +3,15 @@ title = "Home"
 
 
 [[blocks]]
+block_headline = "Strengths"
 headline_1 = "Web Development"
 support_text_1 = "I deliver high-end quality sites by attending to the nuances of optimization, responsiveness, and performance."
 headline_2 = "Analytics"
-support_text_2 = "I create clear, concise reports to clarify the habits and interests of site users."
+support_text_2 = "I create clear, concise reports to understand the habits and interests of site users."
 headline_3 = "Frontend Testing"
 support_text_3 = "Structured testing for all features are a requirement throughout my developing process"
 template = "b-3-column"
 
-[[blocks]]
-skills = ["HTML", "React", "CSS", "SASS", "Analytics", "JavaScript", "JQuery", "Hugo", "Tailwind", "JAMstack"]
-template = "b-hp-skills"
 
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
+
 window.addEventListener('DOMContentLoaded', (e) => {
+
+  slowScroll();
 
   /////////////////////////////////////////////////////////////////////////////////Bamboo Doors
   removeDoors()
-
 
   /////////////////////////////////////////////////////////////////////////////////Mobile Menu
 
@@ -81,7 +83,7 @@ $(document).ready(function(){
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 0.5,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 0,
     cssEase: 'linear',
     speed: 3500,
@@ -140,3 +142,76 @@ $('.work-filters').on( 'click', '.filter', function() {
   $grid.isotope({filter: filter})
 
 });
+
+
+  /////////////////////////////////////////////////////////////////////////////////Slow down scroll
+
+  function slowScroll(){
+    new SmoothScroll(document,12,18)
+  }
+  
+  function SmoothScroll(target, speed, smooth) {
+    if (target === document)
+      target = (document.scrollingElement 
+                || document.documentElement 
+                || document.body.parentNode 
+                || document.body) // cross browser support for document scrolling
+        
+    var moving = false
+    var pos = target.scrollTop
+    var frame = target === document.body 
+                && document.documentElement 
+                ? document.documentElement 
+                : target // safari is the new IE
+    
+    target.addEventListener('mousewheel', scrolled, { passive: false })
+    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+  
+    function scrolled(e) {
+      e.preventDefault(); // disable default scrolling
+  
+      var delta = normalizeWheelDelta(e)
+  
+      pos += -delta * speed
+      pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+  
+      if (!moving) update()
+    }
+  
+    function normalizeWheelDelta(e){
+      if(e.detail){
+        if(e.wheelDelta)
+          return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+        else
+          return -e.detail/3 // Firefox
+      }else
+        return e.wheelDelta/120 // IE,Safari,Chrome
+    }
+  
+    function update() {
+      moving = true
+      
+      var delta = (pos - target.scrollTop) / smooth
+      
+      target.scrollTop += delta
+      
+      if (Math.abs(delta) > 0.5)
+        requestFrame(update)
+      else
+        moving = false
+    }
+  
+    var requestFrame = function() { // requestAnimationFrame cross browser
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(func) {
+          window.setTimeout(func, 1000 / 50);
+        }
+      );
+    }()
+  }
+  
